be89ed86429fcef6c862985dd5c27b33f4187753
dawidchar <dawidchar1@gmail.com>
2021-02-05 13:59:41 +0000

    Chnage Requires into imports

--- Module 1/2 REST Airport API/src/server.js
+++ Module 1/2 REST Airport API/src/server.js
@@ -1,57 +1,58 @@
 const isdev = false
 
-// System
+// Express
+import express from 'express';
+import bodyParser from 'body-parser'
+const app = express();
+
+// Utils and services
 import service from './services/default.service'
 import saveFile from './utils/saveFile.js'
 
-// Express
-const express = require("express");
-const app = express();
-const bodyParser = require('body-parser');
-
 // Airports json
-const airportsFileName = isdev ? "./assets/airportstest.json" : "./assets/airports.json"
+const airportsFileName = isdev ? './assets/airportstest.json' : './assets/airports.json'
 const airports = require(airportsFileName)
 
 // Swagger components
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./assets/docs/APIdocs.json")
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./assets/docs/APIdocs.json')
 
 // Setup Middleware
 app.use(bodyParser.json());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
+
 const saveAirportsData = () => saveFile(airportsFileName, airports)
 
 
 // Main App
 
 // GET - All Airports
-app.get("/airports", (req, res) => {
+app.get('/airports', (req, res) => {
   return service.getAirports(req, res, { airports })
 });
 
 // POST - Add New Airport
-app.post("/airports", (req, res) => {
+app.post('/airports', (req, res) => {
   return service.postAirports(req, res, { airports, saveAirportsData })
 });
 
 // GET - A Single Airport
-app.get("/airport/:icao", (req, res) => {
+app.get('/airport/:icao', (req, res) => {
   return service.getAirport(req, res, { airports })
 });
 
 // PUT - Replace an Aiport's Data
-app.put("/airport/:icao", (req, res) => {
+app.put('/airport/:icao', (req, res) => {
   return service.putAirport(req, res, { airports, saveAirportsData })
 });
 
 // PATCH - Updates an Aiport's Data
-app.patch("/airport/:icao", (req, res) => {
+app.patch('/airport/:icao', (req, res) => {
   return service.patchAirport(req, res, { airports, saveAirportsData })
 });
 
 // DELETE - Delete an Airport
-app.delete("/airport/:icao", (req, res) => {
+app.delete('/airport/:icao', (req, res) => {
   return service.deleteAirport(req, res, { airports, saveAirportsData })
 });
 
