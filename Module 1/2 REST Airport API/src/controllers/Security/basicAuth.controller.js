0f78276c537bf70092c01fb4c9a571d65b2ddecd
dawidchar <dawidchar1@gmail.com>
2021-02-09 11:58:35 +0000

    Basic Auth to use Database for user and password lookup and authentication

--- Module 1/2 REST Airport API/src/controllers/Security/basicAuth.controller.js
+++ Module 1/2 REST Airport API/src/controllers/Security/basicAuth.controller.js
@@ -1,5 +1,13 @@
-function Authorizer(username,password,cb){
-    cb(null,true)
+import bcrypt from 'bcrypt'
+import getUserFromDB from '../../utils/getUserFromDB.service'
+
+async function Authorizer(username, password, cb) {
+    const user = await getUserFromDB(username, { showPassword: true })
+
+    if (!user || !password) return cb(null, false)
+
+    const passwordCorrect = await bcrypt.compare(password, user.password)
+    cb(null, passwordCorrect)
 }
 
 export default Authorizer
--- Module 1/2 REST Airport API/src/models/user.model.js
+++ Module 1/2 REST Airport API/src/models/user.model.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import mongoose from "mongoose"
 const Schema = mongoose.Schema;
 
@@ -11,6 +12,21 @@ const UserSchema = new Schema({
         required: true,
     },
 });
+
+UserSchema.pre("save", function (next) {
+    // get the current user	
+    const user = this;
+
+    // hash the password	
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        if (error) {
+            console.log(error);
+        }
+        user.password = hash;
+        next();
+    });
+});
+
 
 const User = mongoose.model("User", UserSchema, 'Users');
 
